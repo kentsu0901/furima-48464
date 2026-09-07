bec32b2d88cd519c83f75b953fa153f913076a97
kentsu0901 <kentsu.baseball@gmail.com>
2026-09-07 14:50:56 +0900

    Itemモデルのバリデーションテストを追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    product_name { Faker::Commerce.product_name }
+    description { Faker::Lorem.sentence }
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    condition_id { Faker::Number.between(from: 2, to: 7) }
+    shipping_fee_id { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    shipping_day_id { Faker::Number.between(from: 2, to: 4) }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(
+        io: File.open('public/images/test_image.jpeg'),
+        filename: 'test_image.jpeg'
+      )
+    end
   end
 end
