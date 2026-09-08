9e98967682b32f733e37e210374ff53251b7032e
kentsu0901 <kentsu.baseball@gmail.com>
2026-09-08 15:39:28 +0900

    購入情報用Formオブジェクトを実装

---
+++ app/models/order_address.rb
@@ -0,0 +1,27 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :city, presence: true
+  validates :house_number, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+
+    Address.create(
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      house_number: house_number,
+      building_name: building_name,
+      phone_number: phone_number,
+      order_id: order.id
+    )
+  end
+end
