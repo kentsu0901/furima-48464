c8c2b98f8d4c102119f2904766b99f747d817d01
kentsu0901 <kentsu.baseball@gmail.com>
2026-09-08 17:12:20 +0900

    購入情報の保存処理とバリデーションを実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,29 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    @order_address = OrderAddress.new(order_address_params)
+    @item = Item.find(params[:item_id])
+
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(
+      :postal_code,
+      :prefecture_id,
+      :city,
+      :house_number,
+      :building_name,
+      :phone_number
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
