fc75d6312e86036d995df92eadb642b16bc735a5
kentsu0901 <kentsu.baseball@gmail.com>
2026-09-09 12:27:40 +0900

    PAY.JP決済処理を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,6 +11,7 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new(order_address_params)
 
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -28,7 +29,7 @@ class OrdersController < ApplicationController
       :house_number,
       :building_name,
       :phone_number
-    ).merge(user_id: current_user.id, item_id: params[:item_id])
+    ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
@@ -39,5 +40,14 @@ class OrdersController < ApplicationController
     return unless current_user == @item.user || !@item.order.nil?
 
     redirect_to root_path
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      card: @order_address.token, # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,10 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token
 
   validates :user_id, presence: true
   validates :item_id, presence: true
+  validates :token, presence: true
 
   validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
   validates :prefecture_id, numericality: { other_than: 1 }
